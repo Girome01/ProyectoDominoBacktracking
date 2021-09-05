cfa4d6544d1b558746bd16bd4f61c6970dd3229b
Pamela Dinarte <p.dinartech@gmail.com>
2021-09-05 15:38:37 -0600

    Brute Force finished

--- src/proyectodomino/DominoFunctions.java
+++ src/proyectodomino/DominoFunctions.java
@@ -13,5 +13,15 @@ public class DominoFunctions {
         return matriz;
     }
     
+    public static int[][] copyMatriz(int [][] old){
+        int [][] current = new int [old.length][old[0].length];
+        for(int i=0; i<old.length; i++){
+            for(int j=0; j<old[i].length; j++){
+                current[i][j]=old[i][j];
+            }
+        }
+        return current;
+    }
+    
        
 }
